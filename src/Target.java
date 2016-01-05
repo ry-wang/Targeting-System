2ff7754b19ec6ba959c1cdfac6a2ed2ca585aa9e
Ryan Wang <shirui.wang@hotmail.com>
2016-01-04 22:13:24 -0500

    Incorporated paint method onto separate jPanel

--- src/Target.java
+++ src/Target.java
@@ -1,4 +1,6 @@
 import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Dimension;
 import java.awt.EventQueue;
 import java.awt.Graphics;
 import java.awt.event.ActionEvent;
@@ -18,11 +20,13 @@ import javax.swing.SwingConstants;
 public class Target extends JFrame implements ActionListener, ChangeListener{
 
 	private JPanel contentPane;
+	private JPanel pnlContent;
+
 	private int ballSize;
 	private JSlider sldSize;
 	private JSlider sldRange;
 	private int range;
-	
+
 	private Turret turret;
 	private Object objectArray[];
 
@@ -40,8 +44,8 @@ public class Target extends JFrame implements ActionListener, ChangeListener{
 				}
 			}
 		});
-		
-		
+
+
 	}
 
 	/**
@@ -55,17 +59,16 @@ public class Target extends JFrame implements ActionListener, ChangeListener{
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
-		
+
 		JButton btnGenerate = new JButton("Generate");
 		btnGenerate.setBounds(617, 498, 137, 52);
 		contentPane.add(btnGenerate);
 		btnGenerate.addActionListener(this);
 		btnGenerate.setActionCommand("Generate");
-		
-		JPanel pnlContent = new JPanel();
-		pnlContent.setBounds(10, 11, 764, 456);
+
+		pnlContent = new panelContent();
 		contentPane.add(pnlContent);
-		
+
 		sldSize = new JSlider();
 		sldSize.setMinimum(1);
 		sldSize.setMaximum(3);
@@ -73,12 +76,12 @@ public class Target extends JFrame implements ActionListener, ChangeListener{
 		sldSize.setBounds(20, 490, 200, 26);
 		sldSize.addChangeListener(this);
 		contentPane.add(sldSize);
-		
+
 		JLabel lblSize = new JLabel("Size of Target");
 		lblSize.setHorizontalAlignment(SwingConstants.CENTER);
 		lblSize.setBounds(82, 520, 79, 26);
 		contentPane.add(lblSize);
-		
+
 		sldRange = new JSlider();
 		sldRange.setValue(25);
 		sldRange.setMinimum(1);
@@ -86,7 +89,7 @@ public class Target extends JFrame implements ActionListener, ChangeListener{
 		sldRange.setBounds(260, 490, 200, 26);
 		sldRange.addChangeListener(this);
 		contentPane.add(sldRange);
-		
+
 		JLabel lblRange = new JLabel("Range");
 		lblRange.setHorizontalAlignment(SwingConstants.CENTER);
 		lblRange.setBounds(322, 520, 79, 26);
@@ -99,20 +102,42 @@ public class Target extends JFrame implements ActionListener, ChangeListener{
 			generateObjects();
 			turret = new Turret(200, 400, 6, range);
 		}
-		
+
 	}
-	
+
 	public void generateObjects() {
 		int x;
 		int y;
+		String color = "";
+		int c;
+
 		if (sldSize.getValue() == 1) {
-			ballSize = 2;
+			ballSize = 2; // Radius
 		}
 		else if (sldSize.getValue() == 2) {
 			ballSize = 5;
 		}
 		else {
 			ballSize = 8;
+		}
+		for (int i = 0; i < objectArray.length; i++) {
+			x = (int) (Math.random() * 400) + 50;
+			y = (int) (Math.random() * 400) + 50;
+			c = (int) (Math.random() * 5) + 1;
+			switch (c) {
+			case 1: color = "black";
+			break;
+			case 2: color = "blue";
+			break;
+			case 3: color = "green";
+			break;
+			case 4: color = "red";
+			break;
+			case 5: color = "yellow";
+			break;
+			default: color = "black";
+			}
+			objectArray[i] = new Object(x, y, ballSize, color);
 		}
 		setObjectNumber();
 	}
@@ -124,15 +149,23 @@ public class Target extends JFrame implements ActionListener, ChangeListener{
 
 	public void stateChanged(ChangeEvent evt) {
 		if (sldSize == evt.getSource()) {
-			
+
 		}
 		if (sldRange == evt.getSource()) {
 			range = sldRange.getValue();
 		}
-		
+
 	}
 	
-	public void paint(Graphics g) {
-		super.paint(g);
+	class panelContent extends JPanel {
+		panelContent() {
+			this.setBounds(0, 0, 200, 200);
+		}
+		public void paintComponent(Graphics g) {
+			super.paintComponent(g);
+			
+			g.setColor(Color.black);
+			g.drawString("test", 50, 50);
+		}
 	}
 }
